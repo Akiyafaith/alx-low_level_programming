b30f0097c1c8abadd532a1a816b9531310b59afe
Akiyafaith <akiyafaith@gmail.com>
2022-12-15 16:22:58 -0800

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,10 +3,17 @@
 /**
  * _isupper - this is a function to check upper case alphabets
  * @c: an integer parameter
- * Return: 
+ * Return: i if character is uppercase, 0 otherwise. 
  */
 
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }

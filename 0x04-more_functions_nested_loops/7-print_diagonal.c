63160da1cc1553c75a2bb32279ec6d69c2c4206f
Akiyafaith <akiyafaith@gmail.com>
2022-12-16 10:24:42 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print diagonal - this draws a diagonal line 
+ * @n: the mumber of \ character to be drawn
+ */
+void print_diagonal(int n)
+{
+	int line, gap;
+	
+	if (n > 0)
+	{
+	for (line = 0; line <= n; line++)
+	for (gap = 0; gap <= line; gap++)
+	{
+	_putchar(' ');
+	}
+	_putchar('\\');
+	if (line == n - 1)
+	{
+	continue;
+	}
+	_putchar('\n');
+	}
+}
